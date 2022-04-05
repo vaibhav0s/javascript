113b30166d700d76dcb419e300706df6dfdcb45f
vaibhav0s <98278929+vaibhav0s@users.noreply.github.com>
2022-04-06 01:28:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+          //console.log("hello world");
+       //alert("me");
+      // document.write("this is a document write");
+     //  console.warn("this a warning");
+    //  console.error("this an error");
+  //    console.assert(4==(2+6));
+  
+ /*  variables in js
+var number1 =34;
+var number2 =45;
+console.log(number1+number2);
+
+*/
+
+/*
+//    datatypes in javascript
+var num1 = 56;
+var num2 = 54;
+var str1 = "this is string";
+var str2 = "this is  also string";
+var str1 = "this is string";
+var marks = {
+    ravi:34,
+    mukesh:76,
+
+}
+var a = true;
+var b = false;
+console.log(a , b);
+*/
+
+/*
+//var und = undefined;
+var und;
+console.log(undefined);
+
+var n = null;
+console.log(n);
+*/
+
+// at a very high level ,there are two type of datatypes in javascript
+// 1.Premitive data types:undefined,null,Number,String,Boolean,Symbol  
+// 2.reference data types: arrays, objects
+
+//var arr = [1,2,3, "String" ,4,5];
+//console.log(arr)
+//console.log(arr[0])
+//console.log(arr[1])
+//console.log(arr[2])
+//console.log(arr[3])
+//console.log(arr[4])
+ 
+
+//operators in javascript
+//arithmatic oprators
+/*
+var a = 56;
+var b = 56;
+console.log("the value of a+b is:",a+b);
+console.log("the value of a+b is:",a-b);
+console.log("the value of a+b is:",a*b);
+console.log("the value of a+b is:",a/b);
+console.log("the value of a+b is:",a%b);
+*/
+
+
+//Assignment operators
+
+/*
+var c = b;
+ c %= 2;
+ c /= 2;
+ c *= 2;
+ c -= 2;
+ c += 8;
+console.log(c);*/
+
+
+//comparison operator
+var x = 11;
+var y = 23;
+ console.log(x==y);
+ console.log(x<=y);
+ console.log(x>=y);
+ console.log(x<y);
+
+    
